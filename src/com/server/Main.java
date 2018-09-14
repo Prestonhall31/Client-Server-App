6b8e016c121157643b67c2deb12363e4de28a6ff
Preston Hall <prestonhall31@gmail.com>
2018-09-14 16:39:29 -0700

    Initial commit

---
+++ src/com/server/Main.java
@@ -0,0 +1,17 @@
+package com.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    try(ServerSocket serverSocket = new ServerSocket(5000)) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected");
+        } catch (IOException e) {
+	        e.printStackTrace();
+        }
+    }
+}
